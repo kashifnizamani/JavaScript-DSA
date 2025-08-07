4396943c97e09fb8f2eef6cd0a2924743eecb629
Kashif <nizamanikashif2003@gmail.com>
2025-08-08 01:24:22 +0500

    added methods to append, prepend a node. also a toString method

--- Linked_Lists/LinkedList.js
+++ Linked_Lists/LinkedList.js
@@ -1,6 +1,6 @@
-import { node } from "./node";
+import { node } from "./node.js";
 
-class LinkedList{
+ export class LinkedList{
     _head;
 
     constructor(){
@@ -12,5 +12,50 @@ class LinkedList{
         return this._head == null;
 
     }
+    addFirst(value){
+
+        this._head = new node(value);
+
+    }
+    append(value){
+        
+        if(this._head == null){
+            this.addFirst(value);
+        }
+        else{
+            let temp = this._head;
+            while(temp.next != null){
+                temp = temp.next;
+            }
+            temp.next = new node(value);
+        }
+
+    }
+
+    prepend(value){
+        if(this._head == null){
+            this.addFirst(value);
+        }
+        else{
+           
+           let temp = this._head;
+           this.addFirst(value);
+           this._head.next = temp;
+        }
+    }
+
+    toString(){
+        let temp = this._head;
+        let string = "";
+        while(temp != null){
+
+            string += "( " + temp.value + " )" + " -> ";
+            temp = temp.next;
+
+        }
+        string += "null"
+        return string;
+    }
+
   
 }
---
+++ Linked_Lists/main.js
@@ -0,0 +1,11 @@
+import { LinkedList } from "./LinkedList.js";
+
+
+let head = new LinkedList();
+head.append("kashif");
+head.append("22sw052");
+head.prepend("hello");
+head.append("goodbye")
+head.prepend("hi");
+
+console.log(head.toString());
