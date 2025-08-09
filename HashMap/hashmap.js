315ade6927bb2b80c10ec386ea9f9e388e07a619
Kashif <nizamanikashif2003@gmail.com>
2025-08-09 20:44:56 +0500

    added methods has to check key availability, and remove to remove entry

--- HashMap/hashmap.js
+++ HashMap/hashmap.js
@@ -1,40 +1,104 @@
-export const hashmap =  (capacity, loadFactor) =>{
+const Node = (key, value) => {
+  return {
+    key,
+    value,
+    next: null,
+  };
+};
 
-    let arr = new Array(capacity);
+export const hashmap = (capacity, loadFactor) => {
+  let arr = new Array(capacity);
 
-  
-    const hash = (key)=> {
-   let hashCode = 0;
-      
-   const primeNumber = 31;
-   for (let i = 0; i < key.length; i++) {
-     hashCode = primeNumber * hashCode + key.charCodeAt(i);
-     
-   }
+  const hash = (key) => {
+    let hashCode = 0;
 
-   return hashCode;
- }
-
- function set(key, value){
-          let index = hash(key)%capacity;
-          if(arr[index] == null || arr[index].key == key){
-          arr[index] = {key: key, value: value}
-          }
-       
-        
-
-  }
-
-  function get(key){
-    let index = hash(key)%capacity;
-    if(arr[index] == null){
-        return null;
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
     }
-    return arr[index].value;
-  }
 
-return {
-    arr, set, get
-}
+    return hashCode;
+  };
 
-}
+  const set = (key, value) => {
+    let index = hash(key) % capacity;
+
+    if (!arr[index]) {
+      arr[index] = Node(key, value);
+      return;
+    }
+
+    let current = arr[index];
+    while (true) {
+      if (current.key === key) {
+        current.value = value;
+        return;
+      }
+      if (!current.next) break;
+      current = current.next;
+    }
+    current.next = Node(key, value);
+  };
+
+  const get = (key) => {
+    let index = hash(key) % capacity;
+    let current = arr[index];
+    while (current) {
+      if (current.key === key) {
+        return current.value;
+      }
+      current = current.next;
+    }
+    return null;
+  };
+
+  const has = (key) => {
+    let index = hash(key) % capacity;
+
+    let current = arr[index];
+    while (current) {
+      if (current.key === key) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  };
+
+  const remove = (key) => {
+    let index = hash(key) % capacity;
+
+    let current = arr[index];
+
+    if (!current) {
+      return false;
+    }
+    if (!current.next && current.key === key) {
+      arr[index] = null;
+      return true;
+    }
+    let prev = null;
+    while (current) {
+      if (current.key === key) {
+        if (prev) {
+          prev.next = current.next;
+        } else {
+          arr[index] = current.next;
+        }
+        return true;
+      }
+      prev = current;
+      current = current.next;
+    }
+
+    return false;
+  };
+
+  return {
+    arr,
+    set,
+    get,
+    has,
+    remove,
+  };
+};
--- HashMap/main.js
+++ HashMap/main.js
@@ -5,8 +5,3 @@ let hash = hashmap(16,0.8);
 hash.set("carlos", "hello carlos");
 hash.set("carlos", "goodbye carlos");
 hash.set("kashif", "22sw052")
-
-
-
-console.log(hash.arr);
-console.log(hash.get("carlos"));
